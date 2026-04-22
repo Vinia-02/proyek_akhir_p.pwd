5f083d5512dedb5d50c5a1462c225deeb30df447
RAINSSER <raissaceva0180@gmail.com>
2026-04-22 21:53:15 +0700

    update 14

--- config/session.php
+++ config/session.php
@@ -1,14 +1,5 @@
 <?php
 session_start();
 
-$timeout = 30 * 60;
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $timeout)) {
-    session_destroy();
-    header("Location: login.php");
-    exit();
-}
-$_SESSION['last_activity'] = time();
 
-function set_remember_cookie($id_user){
-}
 ?>
