8f6bb08bbdf18b1665701bc118469d50d8f0d3ce
RAINSSER <raissaceva0180@gmail.com>
2026-04-23 19:58:58 +0700

    update 19

--- config/session.php
+++ config/session.php
@@ -1,5 +1,21 @@
 <?php
 session_start();
+require_once 'connect.php';
 
+if (isset($_POST['regis'])) {
+    $nama_pengguna = $_POST['usn'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['pw'], PASSWORD_DEFAULT);
+
+    $checkemail = $koneksi->query("SELECT email FROM pengguna WHERE email = '$email'");
+    if ($checkemail->num_rows > 0){
+        $_SESSION['register_error'] = 'Email sudah terdaftar!';
+    } else {
+        $koneksi->query("INSERT INTO pengguna (nama_pengguna, email, password) VALUES ('$nama_pengguna', '$email', '$password')");
+    }
+
+    header("Location: regis.php");
+    exit();
+}
 
 ?>
