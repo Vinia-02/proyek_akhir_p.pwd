ef2b6fd7207b8ee54e4a6b50bf08842868202516
RAINSSER <raissaceva0180@gmail.com>
2026-04-23 18:44:19 +0700

    update 15

--- donation.php
+++ donation.php
@@ -18,6 +18,8 @@
                     <li><a href="#about">About Us</a></li>
                     <li><a href="projects.php">Projects</a></li>
                     <li><a href="donation.php">Donation</a></li>
+                    <span class="divider">|</span>
+                    <li><a href="login.php">Logout</a></li>
                 </ul>
             </div>
         </div>
